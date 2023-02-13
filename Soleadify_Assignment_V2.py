2165c0d95bc79832f07541e501c6fd93ec6ee05f
HemisCoding <117861750+HemisCoding@users.noreply.github.com>
2023-02-13 09:16:23 +0200

    Second_V2

--- Soleadify_Assignment_V2.py
+++ Soleadify_Assignment_V2.py
@@ -1,4 +1,3 @@
-
 # This script defines a function called scrape_websites that scrapes information from a list of URLs and writes the results to a CSV file.
 
 # The function takes two arguments: parquet_file and output_file, which are the input and output files, respectively.
@@ -7,7 +6,10 @@
 # For each URL, the function sends a GET request using the requests module, and searches the response text for information using a set of regular expressions.
 # Specifically, it searches for the country, city, postcode, and road number using different regular expression patterns.
 
-# The scraped information is printed to the console and also stored in a list of dictionaries, which is later converted into a Pandas DataFrame and saved to a CSV file.
+# The scraped information is printed to the console and also stored in a list of dictionaries, which is later converted into a Pandas DataFrame and saved to a CSV file
+# (the method used to save data to a csv file was a little bit different now).
+
+# The program crashes after some trials because of a connection problem, but it succesfully extracts some postcodes making me understand that the code should be fine.
 
 import re
 import pandas as pd
@@ -33,32 +35,30 @@ def scrape_websites(parquet_file, output_file):
 
         # Define regular expressions to match the patterns of the country, postcode, and road number
         country_patterns = [
-            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 1
-            re.compile(r'<strong>([A-Za-z ]+)</strong>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 2
-            re.compile(r'<h1 class="page-title">([A-Za-z ]+)</h1>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 3
-            re.compile(r'Country: ([A-Za-z ]+)</div>'), # pattern 4
-            re.compile(r'<span class="country-name">([A-Za-z ]+)</span>'), # pattern 5
+            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), 
+            re.compile(r'<strong>([A-Za-z ]+)</strong>[\s\S]*<span>([A-Za-z ]+)</span>'), 
         ]
-        
         postcode_patterns = [
             re.compile(r'\b\d{5}(?:-\d{4})?\b'),
-            re.compile(r'<span>(\d{5})</span>'), # pattern 1
-            re.compile(r'(\d{5})[A-Za-z ]+'), # pattern 2
-            re.compile(r'[A-Za-z ]+(\d{5})'), # pattern 3
-            re.compile(r'([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})'), # pattern 4
+            re.compile(r'<span>(\d{5})</span>'), 
         ]
         road_number_patterns = [
-            re.compile(r'<span>(\d+ [A-Za-z ]+)</span>'), # pattern 1
-            re.compile(r'<span>([A-Za-z ]+ \d+)</span>'), # pattern 2
+            re.compile(r'<span>(\d+ [A-Za-z ]+)</span>'), 
+            re.compile(r'<span>([A-Za-z ]+ \d+)</span>'), 
         ]
+
         city_patterns = [
-            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 1
-            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 2
-            re.compile(r'<h1 class="page-title">([A-Za-z ]+)</h1>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 3
-            re.compile(r'<strong>([A-Za-z ]+)</strong>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 4
-            re.compile(r'<title>([A-Za-z ]+)</title>[\s\S]*<span>([A-Za-z ]+)</span>'), # pattern 5
-            re.compile(r'<span class="locality">([A-Za-z ]+)</span>'), # pattern 3
+            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), 
+            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), 
         ]
+
+        street_name_patterns = [
+            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), 
+            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), 
+            re.compile(r'<span>([A-Za-z ]+) ([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+)</span>'), 
+            re.compile(r'<span>([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+) ([A-Za-z ]+)</span>'), 
+            re.compile(r'<span>([A-Za-z ]+) ([A-Za-z ]+)</span>[\s\S]*<span>([A-Za-z ]+) ([A-Za-z ]+)</span>'), 
+        ]       
 
         # Search for matches using the regular expressions
         country_match = None
@@ -85,26 +85,31 @@ def scrape_websites(parquet_file, output_file):
             if city_match:
                 city_match = city_match[0]
                 break
+        street_name_match = None
+        for pattern in street_name_patterns:
+            street_name_match = pattern.findall(html)
+            if street_name_match:
+                street_name_match = street_name_match[0]
+                break
 
         # Extract the relevant text using the `.group()` method
         country = country_match[0] if country_match else ''
         city = city_match[0] if city_match else ''
         postcode = postcode_match if postcode_match else ''
         road_number = road_number_match if road_number_match else ''
+        street_name = ', '.join(filter(None, street_name_match)) if street_name_match else ''
 
         # Print the scraped data
         print(f"URL: {url}")
         print(f"Country: {country}")
         print(f"City: {city}")
         print(f"Postcode: {postcode}")
+        print(f"Road Name: {street_name}")
         print(f"Road number: {road_number}")
         print()
 
         # Store the scraped data in a dictionary
-        data.append({'url': url, 'country': country, 'city': city, 'postcode': postcode, 'road_number': road_number})
-
-    # Store the scraped data in a dictionary
-        data.append({'url': url, 'country': country, 'city': city, 'postcode': postcode, 'road_number': road_number})
+        data.append({'url': url, 'country': country, 'city': city, 'postcode': postcode, 'road_number': road_number, 'street_name': street_name})
 
     # Convert the list of dictionaries to a Pandas DataFrame
     df = pd.DataFrame(data)
@@ -116,4 +121,4 @@ def scrape_websites(parquet_file, output_file):
     return df
 
 dataframe = scrape_websites(r'C:\Users\User\Desktop\test\list of company websites.snappy.parquet', 'output_file.csv')
-print(dataframe) 
+print(dataframe)
